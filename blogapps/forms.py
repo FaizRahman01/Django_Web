ac8d6596ee816238292835474de48ebfe194893a
faiz <mdfaiz.rahman@gmail.com>
2023-08-21 12:36:16 +0800

    implement style to django form

--- blogapps/forms.py
+++ blogapps/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
 from .models import UserPost
+from django import forms
 
 class UserPostForm(ModelForm):
     class Meta:
         model = UserPost
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'desc': forms.Textarea(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+        
