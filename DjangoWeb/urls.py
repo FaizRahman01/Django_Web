a027183f86ebaee3df02b096e1f6424ef4d5d974
faiz <mdfaiz.rahman@gmail.com>
2023-08-22 12:51:50 +0800

    add path for userapps

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogapps.urls')),
+    path('post/', include('blogapps.urls')),
+    path('', include('userapps.urls')),
 ]
