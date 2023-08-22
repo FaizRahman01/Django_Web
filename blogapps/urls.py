b15abb394cd3e4991ee4ed0562267e3bfddbf4c7
faiz <mdfaiz.rahman@gmail.com>
2023-08-22 12:54:27 +0800

    change path for blogapps

--- blogapps/urls.py
+++ blogapps/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='blog'),
-    path('post/view/<str:post_id>', views.show_post, name='showpost'),
-    path('post/create', views.create_post, name='createpost'),
-    path('post/edit/<str:post_id>', views.edit_post, name='editpost'),
+    path('show/', views.index, name='blog'),
+    path('view/<str:post_id>', views.show_post, name='showpost'),
+    path('create', views.create_post, name='createpost'),
+    path('edit/<str:post_id>', views.edit_post, name='editpost'),
 ]
