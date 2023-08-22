e68eb01f831f0b4da883471aef2300f8997c2629
faiz <mdfaiz.rahman@gmail.com>
2023-08-22 16:18:01 +0800

    create login function

--- userapps/views.py
+++ userapps/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def user_login(request):
+    if(request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            user = None
+            print('User not found')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            print('username or password is incorrect')
+
     return render(request, 'login_user.html')
 
-# Create your views here.
+def user_home(request):
+    return render(request, 'home_user.html')
+
