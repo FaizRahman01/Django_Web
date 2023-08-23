6b70f3277568a11caf2be7c01d68c304d2e0829e
faiz <mdfaiz.rahman@gmail.com>
2023-08-23 17:24:05 +0800

    add user registration form functionality

--- userapps/urls.py
+++ userapps/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
     path('', views.user_home, name='home'),
 ]
--- userapps/views.py
+++ userapps/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def user_login(request):
     if request.user.is_authenticated:
@@ -24,9 +26,27 @@ def user_login(request):
             login(request, user)
             return redirect('home')
         else:
-            print('username or password is incorrect')
+            messages.error(request, "Username or Password is incorrect.")
 
     return render(request, 'login_user.html')
+
+def user_register(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occurred during registration.")
+    register_dict = {'page': page, 'form': form}
+    return render(request, 'login_user.html', register_dict)
+
 
 def user_logout(request):
     logout(request)
