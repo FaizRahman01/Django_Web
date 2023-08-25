a0fa350bf95ac691a6875b165bda63520ce8a358
Faiz Rahman <mdfaiz.rahman@gmail.com>
2023-08-25 16:23:55 +0800

    set nested user serializer to show only id and username

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from blogapps.models import UserPost
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+        #set field to '__all__' will return all user fields(username, email, etc)
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
     class Meta:
         model = UserPost
         fields = '__all__'
